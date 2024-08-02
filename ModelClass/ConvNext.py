ecd0b7bb99b410a6ed642bd84a3ec2253b113f3a
Jae-Man Shin <89717482+shinjaeman1717@users.noreply.github.com>
2024-08-02 16:37:11 +0900

    Update ConvNext.py

--- ModelClass/ConvNext.py
+++ ModelClass/ConvNext.py
@@ -4,6 +4,13 @@ import torch.nn.functional as F
 
 """
 ConvNext Block
+This code is ConvNeXt model. We replaced residual block to ConvNeXt block.
+The code reference is https://github.com/facebookresearch/ConvNeXt
+This code is written by Jae-Man Shin.
+
+Reference:
+1. Liu, Zhuang, et al. "A convnet for the 2020s." Proceedings of the IEEE/CVF conference on computer vision and pattern recognition. 2022.
+
 """
 
 class ConvNeXt_block(nn.Module):
@@ -109,31 +116,3 @@ class Permute(nn.Module):
 
     def forward(self, x):
         return x.permute(self.dims)
-   
-    
-    
-# class LayerNorm(nn.Module):
-#     r""" LayerNorm that supports two data formats: channels_last (default) or channels_first. 
-#     The ordering of the dimensions in the inputs. channels_last corresponds to inputs with 
-#     shape (batch_size, height, width, channels) while channels_first corresponds to inputs 
-#     with shape (batch_size, channels, height, width).
-#     """
-#     def __init__(self, normalized_shape, eps=1e-6, data_format="channels_last"):
-#         super().__init__()
-#         self.weight = nn.Parameter(torch.ones(normalized_shape))
-#         self.bias = nn.Parameter(torch.zeros(normalized_shape))
-#         self.eps = eps
-#         self.data_format = data_format
-#         if self.data_format not in ["channels_last", "channels_first"]:
-#             raise NotImplementedError 
-#         self.normalized_shape = (normalized_shape, )
-    
-#     def forward(self, x):
-#         if self.data_format == "channels_last":
-#             return F.layer_norm(x, self.normalized_shape, self.weight, self.bias, self.eps)
-#         elif self.data_format == "channels_first":
-#             u = x.mean(1, keepdim=True)
-#             s = (x - u).pow(2).mean(1, keepdim=True)
-#             x = (x - u) / torch.sqrt(s + self.eps)
-#             x = self.weight[:, None, None] * x + self.bias[:, None, None]
-#             return x
